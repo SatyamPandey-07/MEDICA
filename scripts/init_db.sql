f11f6f37972245e24f245d57950b4a6c7f376590
ayu. <aezyus1@gmail.com>
2026-05-28 16:12:24 +0530

    knowledge updated
    Signed-off-by: ayu. <aezyus1@gmail.com>

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,5 +1,6 @@
 -- MEDICA PostgreSQL Initialization Script
 -- Creates pgvector extension and sets up schemas
+-- Setup DB
 
 CREATE EXTENSION IF NOT EXISTS vector;
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
