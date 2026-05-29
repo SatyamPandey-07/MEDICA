1561a89520bead74a6597e0481e42774d45a1765
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2026-05-29 13:43:48 +0530

    Revert "feat:Ingestion engine Integration"
    This reverts commit a9505a2c17e07bcf5fbc64d4a1ba191392b125c5.

--- ingestion_agent/app/db/base.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-
-class Base(DeclarativeBase):
-    """
-    Shared Declarative Base for the Ingestion Agent's SQLAlchemy models.
-    All models must inherit from this to be tracked by the session and migrations.
-    """
-    pass
